44bed7f2c466403ba7986dce262efc7bc64c0892
altheaa24 <aeluriz.student@ua.edu.ph>
2025-08-19 15:02:43 +0800

    initial commit

--- components/layout.js
+++ components/layout.js
@@ -4,12 +4,12 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
  
-const name = 'Eaxx.';
+const name = 'Althea Luriz.';
 export const siteTitle = 'Next.js Sample Website';
  
 export default function Layout({ children, home }) {
   return (
-    <div className={styles.container}>
+    <div className={styles.container}>  
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <meta
