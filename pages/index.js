c51c50d248ceead3dcabb8b7fd1f2eda9ecd0d42
altheaa24 <aeluriz.student@ua.edu.ph>
2025-08-19 15:05:41 +0800

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -9,10 +9,10 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Hello My name is Althea Luriz, I am 20years old and I am currently living in Jaen Nueva Ecija. My hobbies are reading wattpad books, playing guitar, and playing chess. 
+        <p>Hello My name is Althea Luriz, I am 20years old and I am currently living in Jaen Nueva Ecija. My hobbies are reading wattpad books, playing guitar, and playing chess. 
           When it comes to my favorite music, I love listening to songs that keep me motivated and inspired just like old love by yuji. My favorite food is chicken curry something simple yet comforting this dishes
           remind me of home and give me a sense of happiness while eating it. In five years, I see myself as a skilled and confident IT professional, hopefully working in the field of 
-          full stack development. ]</p>
+          full stack development.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
